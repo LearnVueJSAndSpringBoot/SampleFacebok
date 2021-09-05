bc13b39e5fe114572fac9767ca02fa9db9f8ece2
tranvutruong <truongtv1399it@gmail.com>
2021-09-05 22:23:16 +0700

    update code

--- backend/src/main/java/com/example/SampleFacebook/controller/PostController.java
+++ backend/src/main/java/com/example/SampleFacebook/controller/PostController.java
@@ -4,6 +4,8 @@ import com.example.SampleFacebook.common.entity.ResponseBase;
 import com.example.SampleFacebook.entity.Post;
 import com.example.SampleFacebook.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,9 @@ public class PostController {
 
     @GetMapping(path = "/getnewpost")
     public ResponseEntity<ResponseBase> getNewPost(){
-        postService.getPostHome();
-        return null;
+        ResponseBase responseBase = new ResponseBase();
+        responseBase.setData(postService.getPostHome());
+        responseBase.setStatus(HttpStatus.OK.value());
+        return new ResponseEntity<>(responseBase,HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/example/SampleFacebook/service/impl/PostServiceImpl.java
+++ backend/src/main/java/com/example/SampleFacebook/service/impl/PostServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.SampleFacebook.service.impl;
 
+import com.example.SampleFacebook.entity.Comment;
 import com.example.SampleFacebook.entity.Post;
 import com.example.SampleFacebook.repository.PostRepository;
 import com.example.SampleFacebook.service.PostService;
+import com.example.SampleFacebook.vo.CommentVO;
 import com.example.SampleFacebook.vo.PostVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,7 +13,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -23,6 +27,48 @@ public class PostServiceImpl implements PostService {
         Pageable pageable = PageRequest.of(0,10, Sort.by("datePost").descending());
         List<Post> posts = postRepository.findAll(pageable).stream().toList();
         List<PostVO> postVOS = new ArrayList<>();
-        return null;
+        for(Post post : posts){
+            PostVO postVO = new PostVO();
+            postVO.setId(post.getId());
+            postVO.setUser(post.getUser().getName());
+            postVO.setTimePost(post.getDatePost());
+            postVO.setContentPost(post.getContent());
+            postVO.setCommentVOS(getComment(post.getComments().stream().toList()));
+            postVOS.add(postVO);
+        }
+        return postVOS;
+    }
+
+    private List<CommentVO> getComment(List<Comment> comments){
+        List<CommentVO> commentVOS = new ArrayList<>();
+        for(int i = 0 ; i < comments.size(); i++){
+            if(comments.get(i).getLevel()==1){
+                CommentVO commentVO = new CommentVO();
+                commentVO.setId(comments.get(i).getId());
+                commentVO.setContent(comments.get(i).getContent());
+                commentVO.setTimeComment(comments.get(i).getDateComment());
+                commentVO.setUser(comments.get(i).getUser().getName());
+                commentVO.setLevel(comments.get(i).getLevel());
+                commentVO.setCommentVOS(getCommentChild(commentVO.getId(),comments.stream().filter(comment -> commentVO.getLevel().intValue() < comment.getLevel().intValue()).toList()));
+                commentVOS.add(commentVO);
+            }
+        }
+        return commentVOS;
+    }
+    private List<CommentVO> getCommentChild(Long idParent,List<Comment> comments){
+        List<CommentVO> commentVOS = new ArrayList<>();
+        for(int i = 0 ; i < comments.size(); i++){
+            if(comments.get(i).getIdParent().equals(idParent)){
+                CommentVO commentVO = new CommentVO();
+                commentVO.setId(comments.get(i).getId());
+                commentVO.setContent(comments.get(i).getContent());
+                commentVO.setTimeComment(comments.get(i).getDateComment());
+                commentVO.setUser(comments.get(i).getUser().getName());
+                commentVO.setLevel(comments.get(i).getLevel());
+                commentVO.setCommentVOS(getCommentChild(commentVO.getId(),comments.stream().filter(comment -> commentVO.getLevel().intValue() < comment.getLevel().intValue()).toList()));
+                commentVOS.add(commentVO);
+            }
+        }
+        return commentVOS;
     }
 }
